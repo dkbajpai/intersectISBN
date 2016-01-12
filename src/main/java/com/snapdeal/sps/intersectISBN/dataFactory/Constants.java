3f5a80321a44fca4c95476571b94a3a23d7c94f1
ashishprasher <ashish.prasher@snapdeal.com>
2016-01-13 02:41:52 +0530

    c

--- src/main/java/com/snapdeal/sps/intersectISBN/dataFactory/Constants.java
+++ src/main/java/com/snapdeal/sps/intersectISBN/dataFactory/Constants.java
@@ -6,7 +6,7 @@ public class Constants {
 
 	public static final int BATCHSIZE = 50000;
 	
-	public static final String INPUT_TXT_DIRECTORY_PATH = "";
+	public static final String INPUT_TXT_DIRECTORY_PATH = "/home/ashish/Documents/f";
 	
 	public static final String WORKING_DIRECTORY =  "/home/ashish/abc/";
 	
@@ -16,10 +16,10 @@ public class Constants {
 	
 	public static final String REJECTED_FILES_DIRECTORY = "rejected";
 	
-	public static final String BINDING_MAP_EXCEL_PATH = "";
+	public static final String BINDING_MAP_EXCEL_PATH = "/home/ashish/Documents/bindingmap.xlsx";
 	
-	public static final String RESTRICTED_BINDING_EXCEL_PATH = "";
+	public static final String RESTRICTED_BINDING_EXCEL_PATH = "/home/ashish/Documents/RestrictedBinding.xlsx";
 	
-	public static final String CATEGORY_MAPPING_EXCEL_PATH = "";
+	public static final String CATEGORY_MAPPING_EXCEL_PATH = "/home/ashish/Documents/101201 BIC2.1 Subj only (2).xls";
 	
 }
