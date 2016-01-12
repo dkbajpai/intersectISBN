9b6d55b27a06fc50f2aed803c713eb1527b62d8e
Ashish Prasher <ashish.prasher@snapdeal.com>
2016-01-13 00:34:41 +0530

    dto and data process code

--- src/main/java/com/snapdeal/sps/intersectISBN/dataFactory/DataUtilities.java
+++ src/main/java/com/snapdeal/sps/intersectISBN/dataFactory/DataUtilities.java
@@ -5,6 +5,7 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Iterator;
 import java.util.Map;
+import java.util.Set;
 
 import org.apache.poi.openxml4j.opc.OPCPackage;
 import org.apache.poi.ss.usermodel.Cell;
@@ -15,9 +16,19 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 public class DataUtilities {
 
 	public static Map<String, String> subCategoryCodeSubCategoryMap;
+	public static Map<String, String> bindingMap;
+	public static Set<String> restrictedBindingSet;
+	
+	public static void loadProgramData(){
+		
+		initializeBindingMap(new File(""));
+		initializeSubCategoryCodeSubCategoryMap(new File(""));
+		getRestrictedBinding(new File(""));
+	}
 
-	public static void initializeSubCategoryCodeSubCategoryMap(File file) {
+	private static void initializeSubCategoryCodeSubCategoryMap(File file) {
 		try {
+			
 			System.out
 					.println("Inside  initializeSubCategoryCodeSubCategoryMap().\nGoing to read file:"
 							+ file);
@@ -28,10 +39,80 @@ public class DataUtilities {
 			rowIterator.next();
 			while (rowIterator.hasNext()) {
 				Row row = rowIterator.next();
+				
+				for(int i = 0; i < 2; i++){
+					if(row.getCell(i) == null)
+						row.createCell(i);
+				}
 				Cell bms = row.getCell(0);
 				Cell subCat = row.getCell(4);
 				subCategoryCodeSubCategoryMap.put(bms.getStringCellValue(),
 						subCat.getStringCellValue());
+			}
+			
+			myWorkBook.close();
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private  static void initializeBindingMap(File file) {
+		try {
+			System.out
+					.println("Inside  initializeBindingMap().\nGoing to read file:"
+							+ file);
+			OPCPackage pkg = OPCPackage.open(file);
+			XSSFWorkbook myWorkBook = new XSSFWorkbook(pkg);
+			XSSFSheet mySheet = myWorkBook.getSheetAt(0);
+			Iterator<Row> rowIterator = mySheet.iterator();
+			rowIterator.next();
+			while (rowIterator.hasNext()) {
+				Row row = rowIterator.next();
+				for(int i = 0; i < 2; i++){
+					if(row.getCell(i) == null)
+						row.createCell(i);
+				}
+				Cell originalBinding = row.getCell(0);
+				Cell mappedBinding = row.getCell(1);
+				bindingMap.put(originalBinding.getStringCellValue().toLowerCase(),
+						mappedBinding.getStringCellValue());
+			}
+			
+			myWorkBook.close();
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+	private  static void getRestrictedBinding(File file) {
+		try {
+			System.out
+					.println("Inside  getRestrictedBinding().\nGoing to read file:"
+							+ file);
+			OPCPackage pkg = OPCPackage.open(file);
+			XSSFWorkbook myWorkBook = new XSSFWorkbook(pkg);
+			XSSFSheet mySheet = myWorkBook.getSheetAt(0);
+			Iterator<Row> rowIterator = mySheet.iterator();
+			rowIterator.next();
+			while (rowIterator.hasNext()) {
+				Row row = rowIterator.next();
+				for(int i = 0; i < 1; i++){
+					if(row.getCell(i) == null)
+						row.createCell(i);
+				}
+				Cell binding = row.getCell(0);
+				restrictedBindingSet.add(binding.getStringCellValue().toLowerCase());
 			}
 			
 			myWorkBook.close();
--- src/main/java/com/snapdeal/sps/intersectISBN/utils/GeneralUtils.java
+++ src/main/java/com/snapdeal/sps/intersectISBN/utils/GeneralUtils.java
@@ -1,5 +1,7 @@
 package com.snapdeal.sps.intersectISBN.utils;
 
+import com.snapdeal.sps.intersectISBN.dataFactory.DataUtilities;
+
 public class GeneralUtils {
 
 	public static String getAuthors(String author) {
@@ -7,5 +9,75 @@ public class GeneralUtils {
 			return "";
 		else
 			return author + ", ";
+	}
+	
+	public static String getValidLanguage(String language) {
+
+		if (language.equals(""))
+			return "Unknown";
+
+		else
+			return language;
+
+	}
+
+	public static String getValidBinding(String binding) {
+
+		if(DataUtilities.bindingMap.get(binding.toLowerCase()) != null)
+			return DataUtilities.bindingMap.get(binding.toLowerCase());
+
+		else
+			return "Unknown";
+	}
+	
+	
+	public static String ISBN10toISBN13( String ISBN10 ) {
+		String ISBN13  = ISBN10;
+		ISBN13 = "978" + ISBN13.substring(0,9);
+		int d;
+
+		int sum = 0;
+		for (int i = 0; i < ISBN13.length(); i++) {
+			d = ((i % 2 == 0) ? 1 : 3);
+			sum += ((((int) ISBN13.charAt(i)) - 48) * d);
+		}
+		sum = 10 - (sum % 10);
+		ISBN13 += sum;
+
+		return ISBN13;
+	}
+
+
+	//http://www.mrexcel.com/forum/excel-questions/285670-isbn-13-10-conversion-macro.html
+	//for isbn13 to isbn10 conversion
+
+	public static String isbn13ToIsbn10( String isbn13){
+		String isbn10 = null;
+		int checkSum = 0;
+		int multiplyingFactor = 10;
+		int chechSumDigit;
+
+		isbn13 = isbn13.replace("-", "").replace(" ", "");
+		if(isbn13.length() != 13)
+			return isbn10;
+
+		isbn10 = isbn13.substring(3,12);
+
+
+		for(int i = 0; i < isbn10.length(); i++){
+			checkSum = checkSum + (Character.getNumericValue(isbn10.charAt(i)) * multiplyingFactor--);
+		}
+
+
+		chechSumDigit = (11 - (checkSum % 11)) % 11;
+		if(chechSumDigit == 10)
+			isbn10 = isbn10 + "X";
+
+		else
+			isbn10 = isbn10 + chechSumDigit;
+
+
+
+		return isbn10;
 	}
 }
