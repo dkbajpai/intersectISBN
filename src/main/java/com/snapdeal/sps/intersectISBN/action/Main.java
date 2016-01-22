cd622d41647fa648905d9b03124f5ab827f19b04
dkbajpai <dkbajpai24@gmail.com>
2016-01-22 17:11:04 +0530

    hello

--- src/main/java/com/snapdeal/sps/intersectISBN/action/Main.java
+++ src/main/java/com/snapdeal/sps/intersectISBN/action/Main.java
@@ -1,14 +1,12 @@
 package com.snapdeal.sps.intersectISBN.action;
 
-<<<<<<< HEAD
+
 import com.snapdeal.sps.intersectISBN.dataFactory.Constants;
 import com.snapdeal.sps.intersectISBN.utils.FileUploadFTP;
 import com.snapdeal.sps.intersectISBN.utils.GeneralUtils;
-=======
+
 import com.snapdeal.sps.intersectISBN.classes.Initializer;
 
-
->>>>>>> a6941af82e2e09eb03618083cce590c61761e6be
 
 
 public class Main {
@@ -17,17 +15,12 @@ public class Main {
 
 		Initializer.initialize();
 		
-<<<<<<< HEAD
 		
 		//uploading files 
-		FileUploadFTP.filesToUpload(FileUploadFTP.getPath(Constants.LOCAL_UPLOAD_SHEET_LOCATION),1);
-		FileUploadFTP.filesToUpload(FileUploadFTP.getPath(Constants.LOCAL_REJECTED_SHEET_LOCATION),2);
-		FileUploadFTP.filesToUpload(FileUploadFTP.getPath(Constants.LOCAL_BOOKS_SHEET_LOCATION),3);
 		
 		//ProcessEngine.readAndProcessInputText();
-=======
 		ProcessEngine.readAndProcessInputText();
->>>>>>> a6941af82e2e09eb03618083cce590c61761e6be
+
 		
 		
 
